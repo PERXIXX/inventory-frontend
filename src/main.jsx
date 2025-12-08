2e8133e6193d45abc64c65100ba16576c1eb14bc
Providenci Terry <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 18:44:22 +0700

    Update main.jsx
    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
->>>>>>> 99cb592d6ac78b8dc656e41bb61066cb9371dd5f
